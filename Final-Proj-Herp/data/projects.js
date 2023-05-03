35d833b04cc7f3528e9cbbe5f979817f9c2e6f24
jnewman747 <jill.newman@uky.edu>
2023-05-02 21:46:12 -0400

    Updated data

--- Final-Proj-Herp/data/projects.js
+++ Final-Proj-Herp/data/projects.js
@@ -3,7 +3,7 @@ var projects = {
     "features": [{
         "type": "Feature",
         "properties": {
-            "title": "Crawfish frog acoustic monitoring",
+            "title": "CRAWFISH FROG ACOUSTICS",
             "years": "2015-2017",
             "description": "Crawfish frog populations are in decline throughout their range. Acoustic recording units were set up in western Kentucky to assess the distribution of crawfish frogs.",
             "taxa": {
@@ -17,7 +17,7 @@ var projects = {
     }, {
         "type": "Feature",
         "properties": {
-            "title": "Reforested Mines",
+            "title": "REFORESTED MINES",
             "years": "2022",
             "description": "The Forestry Reclamation Approach (FRA) is an approach for reclaiming coal-mined land to forest. The goal of this project was to determine amphibian communities in different chronosequenses (control, young FRA, old FRA) in West Virginia",
             "taxa": {
@@ -31,7 +31,7 @@ var projects = {
     }, {
         "type": "Feature",
         "properties": {
-            "title": "Mussels",
+            "title": "ENDANGERED MUSSELS",
             "years": "2021-Present",
             "description": "Many populations of native mussels are declining range-wide. A large-scale (KY, WV, OH, IL, NC, GA, and AL) effort involving in-situ experiments is being conducted to examine potential threats to mussels.",
             "taxa": {
@@ -45,7 +45,7 @@ var projects = {
     }, {
         "type": "Feature",
         "properties": {
-            "title": "Snake Fungal Disease",
+            "title": "SNAKE FUNGAL DISEASE",
             "years": "2013-Present",
             "description": "Snake fungal disease is an emerging infectious disease in which snakes develop discolored, crusted lesions on their skin. Researchers swab snakes to determine prevalence of the fungus as well as survival rates of snakes.",
             "taxa": {
@@ -59,7 +59,7 @@ var projects = {
     }, {
         "type": "Feature",
         "properties": {
-            "title": "Hellbenders",
+            "title": "HELLBENDER DISTRIBUTION",
             "years": "2018-2022",
             "description": "Hellbenders are a species of salamander that are imperiled rangewide. The current status of the hellbender is largely unknown in the state of Kentucky. Researchers used environmental DNA (eDNA) to determine presence/absence of this species across the entire state.",
             "taxa": {
@@ -73,7 +73,7 @@ var projects = {
     }, {
         "type": "Feature",
         "properties": {
-            "title": "Streamside Salamanders",
+            "title": "STREAMSIDE SALAMANDERS",
             "years": "2017-2018",
             "description": "Streamside salamanders are a near threatened species with a restricted geographic range, the majority of which is in Kentucky. An occupany analysis was conducted to determine factors (i.e., water chemistry, presence of fish) contributing to distribution and abundance of streamside salamanders.",
             "taxa": {
@@ -87,7 +87,7 @@ var projects = {
     }, {
         "type": "Feature",
         "properties": {
-            "title": "Dispersal of Stream Salamanders",
+            "title": "DISPERSAL OF STREAM SALAMANDERS",
             "years": "2019-2022",
             "description": "Robinson Forest, a teaching, research, and extension forest is located adjacently to the Van Booven WMA (reclaimed surface minelands) in eastern Kentucky. Researchers use mark-recapture methods to compare salamander dispersal in the two different ecosystems.",
             "taxa": {
@@ -101,7 +101,7 @@ var projects = {
     },{
         "type": "Feature",
         "properties": {
-            "title": "Salamander Diets",
+            "title": "SALAMANDER DIETS",
             "years": "2017",
             "description": "Mountaintop-removal often results in changes in water chemistry. Researchers examined effects of conductivity on distribution, abundance, and diet of stream salamanders on the Van Booven WMA reclaimed mine.",
             "taxa": {
